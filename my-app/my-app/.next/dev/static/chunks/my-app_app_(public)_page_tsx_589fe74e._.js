7c65603f5f1727a162f69f3112d5ebffaf95c027
Katigawa23 <kerbymorte28@gmail.com>
2026-05-10 22:37:31 +0800

    Recycle bin done

--- my-app/my-app/.next/dev/static/chunks/my-app_app_(public)_page_tsx_589fe74e._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/my-app_90006187._.js",
-  "static/chunks/07e91_ff4a1669._.js"
-],
-    source: "dynamic"
-});
